78efb627b327d5f60dd085b1b8d36b565aeb105c
VinceYuan <vinsyuan7788@gmail.com>
2021-06-07 10:13:54 +0800

    Enrich testing cases

--- demo-server-web/src/test/java/com/demo/server/application/reflection/TestReflection.java
+++ demo-server-web/src/test/java/com/demo/server/application/reflection/TestReflection.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,7 +25,44 @@ public class TestReflection {
 
     @Test
     public void test() throws Exception {
-        testReflection();
+//        testReflection();
+        testGetType();
+    }
+
+    /**
+     *  Unable to get the actual type argument while list object has been instantiated <br/>
+     *  -- Since the actual type argument has been erased by JVM
+     *  -- Which means the actual type argument can be only retrieved before instantiation (e.g. during compilation)
+     */
+    private void testGetType() {
+
+        List<String> list = Arrays.asList("hello, world!");
+        Class<? extends List> clazz = list.getClass();
+        log.info("=== XXX | class: {} ===", clazz);
+        Class<?> componentType = clazz.getComponentType();
+        log.info("=== XXX | componentType: {} ===", componentType);
+        Type genericSuperclass = clazz.getGenericSuperclass();
+        log.info("=== XXX | genericSuperclass: {} ===", genericSuperclass);
+        Type[] genericInterfaces = clazz.getGenericInterfaces();
+        log.info("=== XXX | genericInterfaces: {} ===", Arrays.asList(genericInterfaces));
+
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            declaredField.setAccessible(true);
+            Class<?> type = declaredField.getType();
+            log.info("=== XXX | declaredField: {} | type: {} ===", declaredField, type);
+            if (type == java.util.List.class) {
+                // 如果是List类型，得到其Generic的类型
+                Type genericType = declaredField.getGenericType();
+                if (genericType != null) {
+                    if (genericType instanceof ParameterizedType) {
+                        ParameterizedType pt = (ParameterizedType) genericType;
+                        Type[] actualTypeArguments = pt.getActualTypeArguments();
+                        log.info("=== XXX | actualTypeArguments: {} ===", Arrays.asList(actualTypeArguments));
+                    }
+                }
+            }
+        }
     }
 
     private void testReflection() throws Exception {
