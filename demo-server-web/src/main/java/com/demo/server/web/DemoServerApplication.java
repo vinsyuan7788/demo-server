2c13afe52d39a1ab46376d9126696b127dd78a5e
VinceYuan <vinsyuan7788@gmail.com>
2021-11-07 21:21:11 +0800

    Independencies adjustment

--- demo-server-web/src/main/java/com/demo/server/web/DemoServerApplication.java
+++ demo-server-web/src/main/java/com/demo/server/web/DemoServerApplication.java
@@ -5,24 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-/**
- *  If Sharding-Sphere is used to split databases or tables: <br/>
- *  -- @SpringBootApplication should be used with {@link DataSourceAutoConfiguration} excluded
- *
- * @author Vince Yuan
- * @date 12/29/2020
- */
 @Slf4j
 @EnableAsync
 @EnableScheduling
 @EnableTransactionManagement
 @MapperScan({ "com.demo.server.dal.mapper" })
-@SpringBootApplication(scanBasePackages = { "com.demo.*" }, exclude = { org.apache.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration.class })
+@SpringBootApplication(scanBasePackages = { "com.demo.*" })
 public class DemoServerApplication {
 
     public static void main(String[] args) {
